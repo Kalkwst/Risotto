0e183af4c45927e65d1c7e028cce95d0581ade3b
Kalkwst <kalafatiskwstas@gmail.com>
2021-11-11 09:38:53 +0200

    :sparkles: Add AllUnique (#4)
    * :sparkles: Add implementation for AllUnique
    
    * :white_check_mark: [test] Add tests for AllUnique
    
    * :memo: [docs] Update README.md with AllUnique
    
    Co-authored-by: kkalafatis <kkalafatis@novibet.com>

---
+++ Risotto.Test/AllUnique.Test.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using Extensions = Risotto.LINQ.LINQExtensions;
+
+namespace Risotto.Test.LINQExtensions
+{
+	class AllUnique
+	{
+		[TestFixture]
+		public class AllUniqueTests
+		{
+			[Test]
+			public void AllUniqueWithZeroElements()
+			{
+				var source = System.Array.Empty<int>();
+
+				Assert.IsTrue(Extensions.AllUnique(source));
+			}
+
+			[Test]
+			public void AllUniqueWithOneElement()
+			{
+				var source = new int[] { 1 };
+
+				Assert.IsTrue(Extensions.AllUnique(source));
+			}
+
+			[Test]
+			public void AllUniqueWithMultipleElements()
+			{
+				var source = new int[] { 1, 2, 3, 4, 5 };
+
+				Assert.IsTrue(Extensions.AllUnique(source));
+			}
+
+			[Test]
+			public void AllUniqueWithInvalidElements()
+			{
+				var source = new int[] { 1, 2, 3, 4, 1 };
+
+				Assert.IsFalse(Extensions.AllUnique(source));
+			}
+
+			[Test]
+			public void AllUniqueByWithUniqueAllElements()
+			{
+				var source = new int[] { 1, 2, 3, 4, 5, 6 };
+
+				Assert.IsTrue(Extensions.AllUnique(source, x => x * x * x));
+			}
+
+			[Test]
+			public void AllUniqueByWithUniqueSomeElements()
+			{
+				var source = new int[] { 1, 2, 3, 1, 5, 6 };
+
+				Assert.IsFalse(Extensions.AllUnique(source, x => x * x * x));
+			}
+		}
+	}
+}
