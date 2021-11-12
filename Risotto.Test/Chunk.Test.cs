4a5e8d53bd786228397197ee9e154ba674ca93e8
Kalkwst <kalafatiskwstas@gmail.com>
2021-11-12 09:34:50 +0200

    :sparkles: Add Chunk (#10)
    * :sparkles: Add implementation for Chunk
    
    * :white_check_mark: [test] Add tests for Chunk
    
    * :memo: [docs] Update README.md
    
    Co-authored-by: kkalafatis <kkalafatis@novibet.com>

---
+++ Risotto.Test/Chunk.Test.cs
@@ -0,0 +1,88 @@
+﻿using NUnit.Framework;
+using Risotto.LINQ;
+using Risotto.Test.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Risotto.Test.LINQExtensions
+{
+	[TestFixture]
+	public class ChunkTests
+	{
+		[Test]
+		public void ChunkZeroSize()
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(() => Array.Empty<object>().Chunk(0));
+		}
+
+		[Test]
+		public void ChunkNegativeSize()
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(() => Array.Empty<object>().Chunk(-1));
+		}
+
+		[Test]
+		public void ChunkNicelyDivisibleSequence()
+		{
+			var result = new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21 }.Chunk(3);
+
+			using var reader = result.GetReader();
+			Assert.That(reader.Read(), Is.EqualTo(new int[] { 0, 1, 1 }));
+			Assert.That(reader.Read(), Is.EqualTo(new int[] { 2, 3, 5 }));
+			Assert.That(reader.Read(), Is.EqualTo(new int[] { 8, 13, 21 }));
+
+			reader.ReadEnd();
+		}
+
+		[Test]
+		public void ChunkNotNicelyDivisibleSequence()
+		{
+			var result = new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21 }.Chunk(4);
+
+			using var reader = result.GetReader();
+			Assert.That(reader.Read(), Is.EqualTo(new int[] { 0, 1, 1, 2 }));
+			Assert.That(reader.Read(), Is.EqualTo(new int[] { 3, 5, 8, 13 }));
+			Assert.That(reader.Read(), Is.EqualTo(new int[] { 21 }));
+
+			reader.ReadEnd();
+		}
+
+		[Test]
+		public void ChunkSequenceWithProjectorTransformation()
+		{
+			var result = new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21 }.Chunk(3, batch => batch.Sum());
+
+			Assert.That(result, Is.EqualTo(new int[] { 2, 10, 42 }));
+		}
+
+		[Test]
+		public void ChunkSequenceGeneratesListsOfChunks()
+		{
+			var result = new int[] { 1, 2, 3 }.Chunk(2);
+
+			using var reader = result.GetReader();
+			Assert.That(reader.Read(), Is.InstanceOf(typeof(IList<int>)));
+			Assert.That(reader.Read(), Is.InstanceOf(typeof(IList<int>)));
+			reader.ReadEnd();
+		}
+
+		[Test]
+		public void ChuckSequenceAreIndependentInstances()
+		{
+			var result = new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21 }.Chunk(4);
+
+			using var reader = result.GetReader();
+
+			var firstInstance = reader.Read();
+			var secondInstance = reader.Read();
+			var thirdInstance = reader.Read();
+
+			reader.ReadEnd();
+
+			Assert.That(firstInstance, Is.EqualTo(new int[] { 0, 1, 1, 2 }));
+			Assert.That(secondInstance, Is.EqualTo(new int[] { 3, 5, 8, 13 }));
+			Assert.That(thirdInstance, Is.EqualTo(new int[] { 21 }));
+		}
+	}
+}
---
+++ Risotto.Test/Utils/EnumerationReader.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Risotto.Test.Utils
+{
+	static class EnumerationReader
+	{
+		public static EnumerationReader<TSource> GetReader<TSource>(this IEnumerable<TSource> source)
+		{
+			if (source == null)
+				throw new ArgumentNullException(nameof(source));
+
+			return new EnumerationReader<TSource>(source);
+		}
+	}
+
+	public class EnumerationReader<TSource> : IDisposable
+	{
+		IEnumerator<TSource> enumerator;
+
+		public EnumerationReader(IEnumerable<TSource> source) : this(GetEnumerator(source)) { }
+
+		public EnumerationReader(IEnumerator<TSource> enumerator) =>
+			this.enumerator = enumerator ?? throw new ArgumentNullException(nameof(enumerator));
+
+		static IEnumerator<TSource> GetEnumerator(IEnumerable<TSource> source)
+		{
+			if (source == null)
+				throw new ArgumentNullException(nameof(source));
+
+			return source.GetEnumerator();
+		}
+
+		public virtual bool TryRead(out TSource value)
+		{
+			EnsureNotDisposed();
+
+			value = default;
+
+			var e = enumerator;
+			if (!e.MoveNext())
+				return false;
+
+			value = e.Current;
+			return true;
+		}
+
+		public TSource Read() =>
+			TryRead(out var result) ? result : throw new InvalidOperationException();
+
+		public virtual void ReadEnd()
+		{
+			EnsureNotDisposed();
+
+			if (enumerator.MoveNext())
+				throw new InvalidOperationException();
+		}
+
+		protected void EnsureNotDisposed()
+		{
+			if (enumerator == null)
+				throw new ObjectDisposedException(GetType().FullName);
+		}
+
+		public virtual void Dispose()
+		{
+			var e = enumerator;
+			if (e == null)
+				return;
+
+			enumerator = null;
+			e.Dispose();
+		}
+	}
+}
