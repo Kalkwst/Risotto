e782011f925ce2d35fe59c9eba7cb82acd5775aa
Kalkwst <kalafatiskwstas@gmail.com>
2021-11-17 12:42:34 +0200

    :sparkles: Add CountOccurrences (#19)
    * :sparkles: [feat] Add CountOccurrences
    
    * :white_check_mark: [test] Add CountOccurrences tests
    
    * :memo: [docs] Update README.md
    
    Co-authored-by: kkalafatis <kkalafatis@novibet.com>

---
+++ Risotto.Test/CountOccurrences.Test.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using Risotto.LINQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Risotto.Test
+{
+	[TestFixture]
+	public class CountOccurrencesTests
+	{
+		[Test]
+		public void CountOccurrencesNullSequence()
+		{
+			int[] array = null;
+			Assert.Throws<ArgumentNullException>(() => array.CountOccurrences(1));
+		}
+
+		[Test]
+		public void CountOccurrencesNullComparer()
+		{
+			string[] array = new string[] { "a", "b", "c" };
+			Assert.Throws<ArgumentNullException>(() => array.CountOccurrences("a", null));
+		}
+
+		[Test]
+		public void CountOccurrencesSimpleSequence()
+		{
+			int[] array = new int[] { 1, 1, 2, 1, 1, 2, 1 };
+			Assert.That(array.CountOccurrences(2) == 2);
+		}
+
+		[Test]
+		public void CountOccurrencesSimpleSequenceCustomComparer()
+		{
+			string[] array = new string[] { "a", "b", "B", "B", "c"};
+			Assert.That(array.CountOccurrences("b", StringComparer.OrdinalIgnoreCase) == 3);
+		}
+	}
+}
