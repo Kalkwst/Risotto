9096f740795ff6d89c5a0247b345c66daa37a478
kkalafatis <kkalafatis@novibet.com>
2021-11-19 18:16:41 +0200

    [ci skip] :memo: Update documentation

--- Risotto/Difference.cs
+++ Risotto/Difference.cs
@@ -14,6 +14,7 @@ namespace Risotto.LINQ
 		/// <param name="target">The target sequence</param>
 		/// <returns>An <see cref="IEnumerable{T}"/> containing all the elements that exist in the <paramref name="source"/> sequence, 
 		/// and not the <paramref name="target"/> sequence, without filtering duplicates.</returns>
+		/// <exception cref="ArgumentNullException"> if <paramref name="source"/> is null.</exception>
 		public static IEnumerable<T> Difference<T>(this IEnumerable<T> source, IEnumerable<T> target)
 		{
 			return Difference(source, target, EqualityComparer<T>.Default);
@@ -27,6 +28,7 @@ namespace Risotto.LINQ
 		/// <param name="target">The target sequence</param>
 		/// <returns>An <see cref="IEnumerable{T}"/> containing all the elements that exist in the <paramref name="source"/> sequence, 
 		/// and not the <paramref name="target"/> sequence, without filtering duplicates.</returns>
+		/// <exception cref="ArgumentNullException"> if <paramref name="source"/> or <paramref name="comparer"/> is null.</exception>
 		public static IEnumerable<T> Difference<T>(this IEnumerable<T> source, IEnumerable<T> target, IEqualityComparer<T> comparer)
 		{
 			if (source == null)
