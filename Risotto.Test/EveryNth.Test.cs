92a1605da626dd4c03225909758ab7bce7f3e064
kkalafatis <kkalafatis@novibet.com>
2021-11-23 09:26:57 +0200

    :white_check_mark: Add tests for EveryNth

---
+++ Risotto.Test/EveryNth.Test.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using Risotto.LINQ;
+using System;
+
+namespace Risotto.Test
+{
+	[TestFixture]
+	public class EveryNthTests
+	{
+		[Test]
+		public void EveryNthNullSourceSequence()
+		{
+			int[] sequence = null;
+			Assert.Throws<ArgumentNullException>(() => sequence.EveryNth(3));
+		}
+
+		[Test]
+		public void EveryNthZeroStep()
+		{
+			int[] sequence = new int[] { 1, 2, 3 };
+			Assert.Throws<ArgumentException>(() => sequence.EveryNth(0));
+		}
+
+		[Test]
+		public void EveryNthNegativeStep()
+		{
+			int[] sequence = new int[] { 1, 2, 3 };
+			Assert.Throws<ArgumentException>(() => sequence.EveryNth(-1));
+		}
+
+		[Test]
+		public void EveryNthEverySecondElement()
+		{
+			int[] sequence = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+			Assert.That(sequence.EveryNth(2), Is.EqualTo(new int[] { 2, 4, 6 }));
+		}
+	}
+}
