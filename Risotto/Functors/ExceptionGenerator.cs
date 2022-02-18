e5b721a768754798b48f5d9402ceff3a7ae3e443
kkalafatis <kkalafatis@novibet.com>
2022-02-18 15:53:07 +0200

    Add ExceptionGenerator

---
+++ Risotto/Functors/ExceptionGenerator.cs
@@ -0,0 +1,18 @@
+﻿namespace Risotto.Functors
+{
+	public class ExceptionGenerator<T> : IGenerator<T>
+	{
+		/// <summary>
+		/// Singleton predicate instace
+		/// </summary>
+		public static readonly IGenerator<T> INSTANCE = new ExceptionGenerator<T>();
+
+		/// <summary>
+		/// Always throws an exception.
+		/// </summary>
+		public T Generate()
+		{
+			throw new GeneratorException("ExceptionFactory invoked");
+		}
+	}
+}
