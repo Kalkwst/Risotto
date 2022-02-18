b395d4c3383a3ba824f8abaf1558d2bcc4f30d62
Kalkwst <kalafatiskwstas@gmail.com>
2022-02-18 15:57:46 +0200

    ✨ Add Generators (#29)
    * Add Generator interface
    
    * Add ExceptionGenerator
    
    * Add ConstantGenerator
    
    Co-authored-by: kkalafatis <kkalafatis@novibet.com>

---
+++ Risotto/GeneratorException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Risotto
+{
+	public class GeneratorException : SystemException
+	{
+		/// <summary>
+		/// Constructs a new <c>GeneratorException</c> without specified
+		/// detail message.
+		/// </summary>
+		public GeneratorException() { }
+
+		/// <summary>
+		/// Constructs a new <c>GeneratorException</c> with specified
+		/// detail message.
+		/// </summary>
+		/// <param name="msg">the error message</param>
+		public GeneratorException(string msg) : base(msg)
+		{
+		}
+	}
+}
---
+++ Risotto/IGenerator.cs
@@ -0,0 +1,24 @@
+﻿namespace Risotto
+{
+	/// <summary>
+	/// Decorates a functor interface implemented by classes that create objects.
+	/// </summary>
+	/// <remarks>
+	/// A <c>Factory</c> creates an object without using an input parameter.
+	/// If an input parameter is required, them <c>Transformer</c> is more appropriate.
+	/// </remarks>
+	/// <remarks>
+	/// Standard implementations of common factories are provided by
+	/// <c>FactoryUtils</c>. These include factories that return a constant, 
+	/// a copy of a prototype or a new instance.
+	/// </remarks>
+	/// <typeparam name="T">the type that the factory creates</typeparam>
+	public interface IGenerator<T>
+	{
+		/// <summary>
+		/// Create a new object.
+		/// </summary>
+		/// <returns>a new object</returns>
+		T Generate();
+	}
+}
