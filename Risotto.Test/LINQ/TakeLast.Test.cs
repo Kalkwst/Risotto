d3ea947323071188e076e546b1bc95e9537f81d1
kkalafatis <kkalafatis@novibet.com>
2022-02-25 19:51:23 +0200

    :white_check_mark: Add tests for TakeLast

---
+++ Risotto.Test/LINQ/TakeLast.Test.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace Risotto.Test.LINQ
+{
+	[TestFixture]
+	public class TakeLastTests
+	{
+		[Test]
+		public void TakeLast()
+		{
+			var sequence = new[] { 12, 34, 56, 78, 910, 1112 };
+			var skimmed = sequence.TakeLast(3);
+
+			Assert.That(skimmed, Is.EqualTo(new int[] { 78, 910, 1112 }));
+		}
+
+		[Test]
+		public void TakeLastSequenceLessThanCount()
+		{
+			var sequence = new[] { 12, 34, 56 };
+			var skimmed = sequence.TakeLast(5);
+
+			Assert.That(skimmed, Is.EqualTo(sequence));
+		}
+
+		[Test]
+		public void TakeLastNegativeCount()
+		{
+			var sequence = new[] { 12, 34, 56 };
+			var skimmed = sequence.TakeLast(-2);
+
+			Assert.That(skimmed, Is.Empty);
+		}
+	}
+}
