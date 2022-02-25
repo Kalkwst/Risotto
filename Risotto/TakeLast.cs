8c0bf1c8d5c859b66e0cc27585b8d911bd4f656f
Kalkwst <kalafatiskwstas@gmail.com>
2022-02-25 19:54:49 +0200

    :sparkles: Add TakeLast (#33)
    * :sparkles: Add TakeLast LINQ extension
    
    * :white_check_mark: Add tests for TakeLast
    
    Co-authored-by: kkalafatis <kkalafatis@novibet.com>

---
+++ Risotto/TakeLast.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Risotto
+{
+	static partial class LINQExtensions
+	{
+		/// <summary>
+		/// Returns the specified number of contiguous elements from the end of a sequence.
+		/// </summary>
+		/// <typeparam name="T">The type of the emelemts of <paramref name="source"/>.</typeparam>
+		/// <param name="source">The sequence to return the last element of.</param>
+		/// <param name="n">The number of elements to return.</param>
+		/// <returns>A <see cref="IEnumerable{T}"/> that contains the specified number of elements from the end of the input sequence.</returns>
+		/// <exception cref="ArgumentNullException">if <paramref name="source"/> is null.</exception>
+		/// <exception cref="ArgumentOutOfRangeException">if <paramref name="n"/> is less than 0.</exception>
+		public static IEnumerable<T> TakeLast<T>(this IEnumerable<T> source, int n)
+		{
+			if (source == null)
+				throw new ArgumentNullException(nameof(source));
+			if(n < 0)
+				throw new ArgumentOutOfRangeException(nameof(n), $"{nameof(n)} must be greater than or equal to 0");
+
+			Queue<T> buffer = new();
+
+			foreach(var item in source)
+			{
+				buffer.Enqueue(item);
+				if (buffer.Count > n)
+					buffer.Dequeue();
+			}
+
+			return buffer;
+		}
+	}
+}
