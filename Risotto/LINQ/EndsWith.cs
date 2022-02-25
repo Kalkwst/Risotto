7c71b7f4321fa4dd00e53418dc7263a79c6d753c
kkalafatis <kkalafatis@novibet.com>
2022-02-25 22:02:22 +0200

    :memo: Add documentation for EndsWith

--- Risotto/LINQ/EndsWith.cs
+++ Risotto/LINQ/EndsWith.cs
@@ -6,11 +6,29 @@ namespace Risotto.LINQ
 {
 	static partial class LINQExtensions
 	{
+		/// <summary>
+		/// Determines whether the end of the first sequence is equivalent to 
+		/// the second sequence, using the default equality comparer.
+		/// </summary>
+		/// <typeparam name="T">The type of the sequence elements.</typeparam>
+		/// <param name="source">The source sequence.</param>
+		/// <param name="sequence">The sequence to compare to.</param>
+		/// <returns><c>true</c> if <paramref name="source"/> ends with elements equivalent to <paramref name="sequence"/>.</returns>
 		public static bool EndsWith<T>(this IEnumerable<T> source, IEnumerable<T> sequence)
 		{
 			return EndsWith(source, sequence, null);
 		}
 
+		/// <summary>
+		/// Determines whether the end of the first sequence is equivalent to
+		/// the second sequence, using the specified element equality comparer.
+		/// </summary>
+		/// <typeparam name="T">The type of the sequence elements.</typeparam>
+		/// <param name="source">The source sequence.</param>
+		/// <param name="sequence">The sequence to compare to.</param>
+		/// <param name="comparer">Equality comparer to use.</param>
+		/// <returns><c>true</c> if <paramref name="source"/> ends with elements equivalent to <paramref name="sequence"/>.</returns>
+		/// <exception cref="ArgumentNullException">if <paramref name="source"/> or <paramref name="sequence"/> is null</exception>
 		public static bool EndsWith<T>(this IEnumerable<T> source, IEnumerable<T> sequence, IEqualityComparer<T>? comparer)
 		{
 			if (source == null)
