65f628d4fbe2980931b6effc10856bb9b4f46e46
kkalafatis <kkalafatis@novibet.com>
2022-03-29 20:49:58 +0300

    :sparkles: Add more IPredicate implementations

---
+++ Risotto/Functors/EqualsPredicate.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Risotto.Functors
+{
+	/// <summary>
+	/// Predicate implementation that returns true if the input is the same object
+	/// as the one stored in this predicate.
+	/// </summary>
+	public class EqualsPredicate<T> : IPredicate<T>
+	{
+		/// <summary>
+		/// The value to compare to
+		/// </summary>
+		private readonly T _value;
+
+		/// <summary>
+		/// The equality comparer to use for the comparison
+		/// </summary>
+		private readonly IEqualityComparer<T> _comparer;
+
+		/// <summary>
+		/// Factory to create the predicate
+		/// </summary>
+		/// <param name="value">The internal value</param>
+		/// <returns>The predicate</returns>
+		public static IPredicate<T> GetEqualPredicate(T value)
+		{
+			if (value == null)
+				return NullPredicate<T>.GetNullPredicate();
+
+			return new EqualsPredicate<T>(value);
+		}
+
+		/// <summary>
+		/// Factory to create the predicate
+		/// </summary>
+		/// <param name="value">The internal value</param>
+		/// <param name="comparer">The comparer to use for comparison</param>
+		/// <returns>The predicate</returns>
+		public static IPredicate<T> GetEqualPredicate(T value, IEqualityComparer<T> comparer)
+		{
+			if (value == null)
+				return NullPredicate<T>.GetNullPredicate();
+
+			return new EqualsPredicate(value, comparer);
+		}
+
+		/// <summary>
+		/// Restrictive constructor 
+		/// </summary>
+		private EqualsPredicate(T value)
+		{
+			_value = value;
+			_comparer = EqualityComparer<T>.Default;
+		}
+
+		/// <summary>
+		/// Restrictive constructor
+		/// </summary>
+		private EqualsPredicate(T value, IEqualityComparer<T> comparer) : this(value)
+		{
+			_comparer = comparer;
+		}
+
+		/// <summary>
+		/// Evaluates the predicate returning true if the input equals the internal value
+		/// </summary>
+		/// <param name="value">the input object</param>
+		/// <returns>true if input object equals the interval value</returns>
+		public bool Evaluate(T value)
+		{
+			return _comparer.Equals(_value, value);
+		}
+
+		/// <summary>
+		/// Get the internal value
+		/// </summary>
+		/// <returns>The internal value</returns>
+		public T GetInternalValue() => _value;
+	}
+}
---
+++ Risotto/Functors/NullPredicate.cs
@@ -0,0 +1,34 @@
+﻿namespace Risotto.Functors
+{
+	/// <summary>
+	/// Predicate implementation that returns true if the input is null.
+	/// </summary>
+	public class NullPredicate<T> : IPredicate<T>
+	{
+		/// <summary>
+		/// Singleton predicate instance
+		/// </summary>
+		public static readonly IPredicate<T> Instance = new NullPredicate<T>();
+
+		/// <summary>
+		/// Factory returning the singleton instance.
+		/// </summary>
+		/// <returns>the singleton instance</returns>
+		public static IPredicate<T> GetNullPredicate() => Instance;
+
+		/// <summary>
+		/// Evaluates the predicate returning true if the input is null
+		/// </summary>
+		/// <param name="value">the input object</param>
+		/// <returns>true if input is null</returns>
+		public bool Evaluate(T value)
+		{
+			return value == null;
+		}
+
+		/// <summary>
+		/// Restricted constructor.
+		/// </summary>
+		private NullPredicate() { }
+	}
+}
