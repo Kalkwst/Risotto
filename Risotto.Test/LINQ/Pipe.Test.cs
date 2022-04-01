a57ea624775146b3b8f056fa79cef84829d2ac1f
Kalkwst <kalafatiskwstas@gmail.com>
2022-04-01 10:25:38 +0300

    :sparkles: Add Pipe (#39)
    * :sparkles: Add Pipe Extension
    
    * :white_check_mark: Add tests for Pipe
    
    Co-authored-by: kkalafatis <kkalafatis@novibet.com>

---
+++ Risotto.Test/LINQ/Pipe.Test.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Risotto.LINQ;
+using System.Collections.Generic;
+
+namespace Risotto.Test.LINQ
+{
+	[TestFixture]
+	public class PipeTests
+	{
+		[Test]
+		public void PipeWithSequence()
+		{
+			var results = new List<int>();
+			var returned = new[] { 1, 2, 3 }.Pipe(results.Add);
+			Assert.That(results, Is.Empty);
+
+			Assert.That(returned, Is.EquivalentTo(new int[] { 1, 2, 3 }));
+			Assert.That(results, Is.EquivalentTo(new int[] { 1, 2, 3 }));
+		}
+	}
+}
