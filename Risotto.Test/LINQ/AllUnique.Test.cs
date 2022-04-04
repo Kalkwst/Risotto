3ab210298c060b76647e7ad97f621f0a77c818d8
Kalkwst <kalafatiskwstas@gmail.com>
2022-04-04 10:37:44 +0300

    ✅ Add additional tests for AllUnique (#44)
    * :white_check_mark: Add test for ExceptionGenerator
    
    * :white_check_mark: Update tests
    
    * :white_check_mark: Update tests on AllUnique
    
    Co-authored-by: kkalafatis <kkalafatis@novibet.com>

--- Risotto.Test/LINQ/AllUnique.Test.cs
+++ Risotto.Test/LINQ/AllUnique.Test.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 using Extensions = Risotto.LINQ.LINQExtensions;
 
 namespace Risotto.Test.LINQExtensions
@@ -54,6 +55,31 @@ namespace Risotto.Test.LINQExtensions
 				var source = new int[] { 1, 2, 3, 1, 5, 6 };
 
 				Assert.IsFalse(Extensions.AllUnique(source, x => x * x * x));
+			}
+
+			[Test]
+			public void AllUniqueByNullSource()
+			{
+				var ex = Assert.Throws<ArgumentNullException>(
+				() => {
+					int[] source = null;
+					Extensions.AllUnique(source);
+				});
+
+				Assert.That(ex.Message, Is.EqualTo("Value cannot be null. (Parameter 'source')"));
+			}
+
+			[Test]
+			public void AllUniqueByNullFunc()
+			{
+				var ex = Assert.Throws<ArgumentNullException>(
+				() => {
+					int[] source = new int[] { 1, 2, 3, 4, 5};
+					Func<int, int> func = null;
+					Extensions.AllUnique(source, func);
+				});
+
+				Assert.That(ex.Message, Is.EqualTo("Value cannot be null. (Parameter 'fn')"));
 			}
 		}
 	}
