9d5cc3b6e761d1a909064eb35ecfe8de3ad2af46
kkalafatis <kkalafatis@novibet.com>
2022-04-07 10:34:24 +0300

    :white_check_mark: Add NotNullPredicate

---
+++ Risotto.Test/Functors/Predicates/NotNullPredicate.Test.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Risotto.Functors;
+
+namespace Risotto.Test.Functors.Predicates
+{
+	[TestFixture]
+	public class NotNullPredicateTest
+	{
+		[Test]
+		public void NotNullPredicateNotNullValue()
+		{
+			var predicate = NotNullPredicate<int>.GetNotNullPredicate;
+			Assert.IsTrue(predicate.Evaluate(12));
+		}
+
+		[Test]
+		public void NotNullPredicateNullValue()
+		{
+			var predicate = NotNullPredicate<string>.GetNotNullPredicate;
+			Assert.IsFalse(predicate.Evaluate(null));
+		}
+	}
+}
