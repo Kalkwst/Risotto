9b9ebf29afa247ba30cac914019fd63df64a813a
kkalafatis <kkalafatis@novibet.com>
2022-04-07 14:08:16 +0300

    :white_check_mark: Add tests for ExceptionPredicate

---
+++ Risotto.Test/Functors/Predicates/ExceptionPredicate.Test.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Risotto.Functors;
+
+namespace Risotto.Test.Functors.Predicates
+{
+	[TestFixture]
+	public class ExceptionPredicateTest
+	{
+		[Test]
+		public void TestExceptionPredicateInstance()
+		{
+			var predicate = ExceptionPredicate<int>.Instance;
+			Assert.That(predicate, Is.Not.Null);
+		}
+
+		[Test]
+		public void ExceptionPredicateAssertFactoryMethod()
+		{
+			var predicate = ExceptionPredicate<int>.GetExceptionPredicate();
+			Assert.That(predicate, Is.Not.Null);
+		}
+
+		[Test]
+		public void ExceptionPredicateAlwaysThrowsException()
+		{
+			var ex = Assert.Throws<FunctorException>(
+				() =>
+				{
+					ExceptionPredicate<int>.GetExceptionPredicate().Evaluate(12);
+				});
+
+			Assert.That(ex.Message, Is.EqualTo("ExceptionPredicate invoked"));
+		}
+	}
+}
