5316aa4c592dd279d34006a3694b79b6a7284234
kkalafatis <kkalafatis@novibet.com>
2022-04-07 11:07:17 +0300

    :white_check_mark: Add tests for AndPredicate

---
+++ Risotto.Test/Functors/Predicates/AndPredicate.Test.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using Risotto.Functors;
+
+namespace Risotto.Test.Functors.Predicates
+{
+	[TestFixture]
+	public class AndPredicateTest
+	{
+		private readonly TruePredicate<int> truePredicate = (TruePredicate<int>)TruePredicate<int>.Instance;
+		private readonly FalsePredicate<int> falsePredicate = (FalsePredicate<int>) FalsePredicate<int>.Instance;
+
+		[Test]
+		public void TestAndPredicateFalseFalse()
+		{
+			var predicate = AndPredicate<int>.GetAndPredicate(falsePredicate, falsePredicate);
+			Assert.IsFalse(predicate.Evaluate(12));
+		}
+
+		[Test]
+		public void TestAndPredicateFalseTrue()
+		{
+			var predicate = AndPredicate<int>.GetAndPredicate(falsePredicate, truePredicate);
+			Assert.IsFalse(predicate.Evaluate(12));
+		}
+
+		[Test]
+		public void TestAndPredicateTrueFalse()
+		{
+			var predicate = AndPredicate<int>.GetAndPredicate(truePredicate, falsePredicate);
+			Assert.IsFalse(predicate.Evaluate(12));
+		}
+
+		[Test]
+		public void TestAndPredicateTrueTrue()
+		{
+			var predicate = AndPredicate<int>.GetAndPredicate(truePredicate, truePredicate);
+			Assert.IsTrue(predicate.Evaluate(12));
+		}
+	}
+}
