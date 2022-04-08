56883501ca92fc2ee81f2b15deb40a2b7bf934c4
kkalafatis <kkalafatis@novibet.com>
2022-04-08 09:31:46 +0300

    :recycle: Simplify true, false and null predicates

--- Risotto/Functors/Predicates/FalsePredicate.cs
+++ Risotto/Functors/Predicates/FalsePredicate.cs
@@ -6,17 +6,6 @@
 	public class FalsePredicate<T> : IPredicate<T>
 	{
 		/// <summary>
-		/// Singleton predicate instance
-		/// </summary>
-		public static IPredicate<T> Instance = new FalsePredicate<T>();
-
-		/// <summary>
-		/// Factory returning the singleton instance.
-		/// </summary>
-		/// <returns></returns>
-		public static IPredicate<T> GetFalsePredicate => Instance;
-
-		/// <summary>
 		/// Evaluates the predicate returning always false.
 		/// </summary>
 		/// <param name="value">the input object</param>
@@ -25,9 +14,5 @@
 		{
 			return false;
 		}
-
-		private FalsePredicate() { }
-
-
 	}
 }
