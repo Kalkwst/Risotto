e53b3f982a42dbbe650b05de8e6fb5f8915bb656
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2022-05-16 16:09:48 +0300

    :sparkles: Add CollectionUtils.IsEmpty

---
+++ Risotto/CollectionsUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Risotto
+{
+    public class CollectionsUtils<T>
+    {
+        /// <summary>
+        /// Null-safe check if the specified collection is empty.
+        /// </summary>
+        /// <param name="coll">the collection to check, may be null</param>
+        /// <returns>true if empty or null</returns>
+        /// <remarks>null returns true</remarks>
+        public static bool IsEmpty(ICollection<T> coll)
+        {
+            return coll == null || coll.Count == 0;
+        }
+    }
+}
+
